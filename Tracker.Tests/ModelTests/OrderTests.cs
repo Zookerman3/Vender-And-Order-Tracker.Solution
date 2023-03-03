8255d77e67a6ba515220b1c83412c0d8ed5cf869
Zookerman3 <szook7@gmail.com>
2023-03-03 12:28:46 -0800

    passed test 7

--- Tracker.Tests/ModelTests/OrderTests.cs
+++ Tracker.Tests/ModelTests/OrderTests.cs
@@ -5,15 +5,28 @@ using System;
 
 namespace Tracker.Tests
 {
-  [TestClass]
-  public class OrderTests
-  {
-
-    [TestMethod]
-    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    [TestClass]
+    public class OrderTests
     {
-      Order newOrder = new Order("order");
-      Assert.AreEqual(typeof(Order), newOrder.GetType());
+
+        [TestMethod]
+        public void OrderConstructor_CreatesInstanceOfOrder_Order()
+        {
+            Order newOrder = new Order("order");
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+        [TestMethod]
+        public void GetDescription_ReturnsDescription_String()
+        {
+            //Arrange
+            string description = "Pastries";
+
+            //Act
+            Order newOrder = new Order(description);
+            string result = newOrder.Description;
+
+            //Assert
+            Assert.AreEqual(description, result);
+        }
     }
-  }
 }
